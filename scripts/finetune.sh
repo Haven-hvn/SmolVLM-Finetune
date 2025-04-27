cd8988dff28ebe41931664861de8e65181e5a827
HavenCTO <jaanon@fedora.email>
2025-04-27 19:05:21 -0400

    Update finetune.sh

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -7,18 +7,18 @@ export PYTHONPATH=src:$PYTHONPATH
 deepspeed src/training/train.py \
     --deepspeed scripts/zero3.json \
     --model_id $MODEL_NAME \
-    --data_path /path/to/your/training/data.json \
-    --image_folder /path/to/your/image/folder \
+    --data_path /workspace/SmolVLM-Finetune/llava_dataset.json \
+    --image_folder /workspace/SmolVLM-Finetune/dataset_images \
     --freeze_vision_tower False \
     --freeze_llm False \
     --tune_connector True \
     --bf16 True \
     --fp16 False \
-    --disable_flash_attn2 False \
+    --disable_flash_attn2 True \
     --output_dir output/testing \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 1 \
     --learning_rate 1e-5 \
     --connector_lr 1e-5 \
     --vision_lr 2e-6 \
@@ -26,11 +26,11 @@ deepspeed src/training/train.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --gradient_checkpointing True \
     --report_to tensorboard \
     --lazy_preprocess True \
     --save_strategy "steps" \
     --save_steps 200 \
     --save_total_limit 10 \
-    --dataloader_num_workers 4
+    --dataloader_num_workers 1
